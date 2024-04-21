10f8144ce1d37a3c80616f0572413e9e2cf7ab51
TL044CN <gunnisan2@web.de>
2024-04-21 10:34:11 +0200

    ADD: Implemented Endpoint
    include/Endpoint.hpp
    - Added includes
    - Added Sockaddr union
    - Added constructors
        - with sockaddr* and socklen_t
        - with std::string and uint16_t
        - with in_addr and uint16_t
        - with sockaddr_storage and socklen_t
        - with AddressFamily and uint16_t
    - Added get methods
        - getAddressFamily
        - getPort
        - c_addr
        - c_size

--- include/Endpoint.hpp
+++ include/Endpoint.hpp
@@ -4,19 +4,107 @@
  * @brief Endpoint Class for SocketSparrow
  * @version 0.1
  * @date 2024-04-20
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
 #pragma once
 
+#include "Enums.hpp"
+
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string>
+
 namespace SocketSparrow {
 
-    /**
-     * @brief Abstraction for a Network Endpoint
-     */
-    class Endpoint {
+/**
+ * @brief Abstraction for a Network Endpoint
+ */
+class Endpoint {
+private:
+    union Sockaddr {
+        sockaddr        base;
+        sockaddr_in     ipv4;
+        sockaddr_in6    ipv6;
+    } mSockaddr;
 
-    };
+    AddressFamily mAddressFamily;    
+
+public:
+    /**
+     * @brief Construct a new Endpoint object
+     * 
+     * @param addr sockaddr to copy
+     * @param size size of sockaddr
+     */
+    Endpoint(sockaddr* addr, socklen_t size);
+
+    /**
+     * @brief Construct a new Endpoint object
+     * 
+     * @param hostname the Hostname of the Endpoint
+     * @param port the Port of the Endpoint
+     */
+    Endpoint(std::string hostname = "localhost", uint16_t port = 80);
+
+    /**
+     * @brief Construct a new Endpoint object
+     * 
+     * @param ip the IP of the Endpoint
+     * @param port the Port of the Endpoint
+     */
+    Endpoint(in_addr_t ip, uint16_t port = 80);
+
+    /**
+     * @brief Construct a new Endpoint object
+     * 
+     * @param addr address to copy
+     * @param size size of address
+     */
+    Endpoint(sockaddr_storage addr, socklen_t size);
+
+    /**
+     * @brief Construct a new Endpoint object
+     * 
+     * @param af the AddressFamily of the Endpoint
+     * @param port the Port of the Endpoint
+     */
+    Endpoint(AddressFamily, uint16_t port = 80);
+
+    /**
+     * @brief Destroy the Endpoint
+     */
+    ~Endpoint();
+
+    /**
+     * @brief Get the AddressFamily of the Endpoint
+     * 
+     * @return AddressFamily the AddressFamily of the Endpoint
+     */
+    AddressFamily getAddressFamily() const;
+
+    /**
+     * @brief Get the Port of the Endpoint
+     * 
+     * @return uint16_t the Port of the Endpoint
+     */
+    int getPort() const;
+
+    /**
+     * @brief get the sockaddr* of the Endpoint
+     * 
+     * @return sockaddr* raw sockaddr struct of the Endpoint
+     */
+    const sockaddr* c_addr() const;
+
+    /**
+     * @brief get the size of the sockaddr struct
+     * 
+     * @return socklen_t size of the sockaddr struct
+     */
+    socklen_t c_size() const;
+
+};
 
 } // namespace SocketSparrow
--- source/Endpoint.cpp
+++ source/Endpoint.cpp
@@ -1,5 +1,60 @@
 #include "Endpoint.hpp"
+#include "Util.hpp"
+
+#include <cstring>
 
 namespace SocketSparrow {
+
+Endpoint::Endpoint(sockaddr* addr, socklen_t size) {
+    memcpy(&mSockaddr.base, addr, size);
+    mAddressFamily = Util::getAddressFamily(mSockaddr.base.sa_family);
+}
+
+Endpoint::Endpoint(std::string hostname, uint16_t port) {
+    mAddressFamily = AddressFamily::IPv4;
+    mSockaddr.ipv4.sin_family = AF_INET;
+    mSockaddr.ipv4.sin_port = htons(port);
+    mSockaddr.ipv4.sin_addr.s_addr = inet_addr(hostname.c_str());
+}
+
+Endpoint::Endpoint(in_addr_t ip, uint16_t port) {
+    mAddressFamily = AddressFamily::IPv4;
+    mSockaddr.ipv4.sin_family = AF_INET;
+    mSockaddr.ipv4.sin_port = htons(port);
+    mSockaddr.ipv4.sin_addr.s_addr = ip;
+}
+
+Endpoint::Endpoint(AddressFamily af, uint16_t port) {
+    mAddressFamily = af;
+    mSockaddr.ipv4.sin_family = Util::getNativeAddressFamily(af);
+    mSockaddr.ipv4.sin_port = htons(port);
+    mSockaddr.ipv4.sin_addr.s_addr = htonl(INADDR_ANY);
+}
+
+Endpoint::Endpoint(sockaddr_storage addr, socklen_t size) {
+    memcpy(&mSockaddr.base, &addr, size);
+    mAddressFamily = Util::getAddressFamily(mSockaddr.base.sa_family);
+}
+
+Endpoint::~Endpoint() {
+
+}
+
+
+AddressFamily Endpoint::getAddressFamily() const {
+    return mAddressFamily;
+}
+
+int Endpoint::getPort() const {
+    return ntohs(mSockaddr.ipv4.sin_port);
+}
+
+const sockaddr* Endpoint::c_addr() const {
+    return &mSockaddr.base;
+}
+
+socklen_t Endpoint::c_size() const {
+    return sizeof(mSockaddr);
+}
 
 }   // namespace SocketSparrow
