57e410d88bfcb7d07f3f7b305d23e729d16f9574
TL044CN <gunnisan2@web.de>
2024-06-01 16:54:21 +0200

    CHG: Using MockingBird Library now instead of internal Mocking

--- tests/SocketMocking.cpp
+++ tests/SocketMocking.cpp
@@ -9,33 +9,40 @@
  * 
  */
 
-#include "Mocking.hpp"
+#include "SocketMocking.hpp"
 #include <arpa/inet.h>
 
 template<typename... Args>
-using MF = MockingBird::MockingController::MockFunction<Args...>;
+using MF = MockingBird::MockingController::MockingFunction<Args...>;
 
 int socket(int domain, int type, int protocol) {
-    static MF<int, int, int, int> mock_socket("socket", ::socket);
+    static MF<int, int, int, int> mock_socket(
+        "socket", MF<int, int, int, int>::fromSyscall("socket")
+    );
 
     return mock_socket(domain, type, protocol);
 }
 
 int listen(int sockfd, int backlog) {
-    static MF<int, int, int> mock_listen("listen", ::listen);
+    static MF<int, int, int> mock_listen(
+        "listen", MF<int, int, int>::fromSyscall("listen")
+    );
 
     return mock_listen(sockfd, backlog);
 }
 
 int accept(int sockfd, struct sockaddr* addr, socklen_t* addrlen) {
-    static MF<int, int, struct sockaddr*, socklen_t*> mock_accept("accept", ::accept);
+    static MF<int, int, struct sockaddr*, socklen_t*> mock_accept(
+        "accept", MF<int, int, struct sockaddr*, socklen_t*>::fromSyscall("accept")
+    );
 
     return mock_accept(sockfd, addr, addrlen);
 }
 
 int setsockopt(int sockfd, int level, int optname, const void* optval, socklen_t optlen) {
-    static MF<int, int, int, int, const void*, socklen_t>
-        mock_setsockopt("setsockopt", ::setsockopt);
+    static MF<int, int, int, int, const void*, socklen_t> mock_setsockopt(
+        "setsockopt", MF<int, int, int, int, const void*, socklen_t>::fromSyscall("setsockopt")
+    );
 
     return mock_setsockopt(sockfd, level, optname, optval, optlen);
 }
--- tests/SocketMocking.hpp
+++ tests/SocketMocking.hpp
@@ -10,4 +10,4 @@
  */
 #pragma once
 
-#include "Mocking.hpp"
+#include "MockingBird.hpp"
