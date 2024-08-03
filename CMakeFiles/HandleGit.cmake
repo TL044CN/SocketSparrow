3c411f714992d695cfdf69559ed686952e50d044
TL044CN <gunnisan2@web.de>
2024-08-03 14:56:28 +0200

    CHG: Improved robustnes of automated Tag-based Versioning

--- CMakeFiles/HandleGit.cmake
+++ CMakeFiles/HandleGit.cmake
@@ -1,11 +1,63 @@
 find_package(Git)
+# PROJECT_REVISION is the version tag that points to the latest tagged commit,
+# sanitized to only be numbers delimited by ".".
+set(PROJECT_REVISION "" CACHE INTERNAL "")
+
 if(GIT_FOUND)
+    # find the correct version tag, given a text tag (e.g. experimental, latest) or version tag.
+    # If the textTag is not a version tag (e.g. v1.0.0), then it will find the version tag
+    # that points to the commit hash. The version tag is then stored in PROJECT_REVISION.
+    function(findVersionTag textTag)
+        set(VERSION_REGEX "^[vV]?[0-9]+(\.[0-9]+)*([a-zA-Z]*)$")
+        string(REGEX MATCH ${VERSION_REGEX} MATCHED_VERSION ${textTag})
+
+        # if not a version tag, find the version tag that points to the commit hash.
+        if(NOT MATCHED_VERSION)
+            execute_process(
+                COMMAND ${GIT_EXECUTABLE} rev-list -n 1 ${textTag}
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                OUTPUT_VARIABLE GIT_COMMIT_HASH
+                OUTPUT_STRIP_TRAILING_WHITESPACE
+            )
+
+            # get all tags that point to the commit hash
+            execute_process(
+                COMMAND ${GIT_EXECUTABLE} tag --points-at ${GIT_COMMIT_HASH}
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                OUTPUT_VARIABLE TAGS_STRING
+                OUTPUT_STRIP_TRAILING_WHITESPACE
+            )
+            string(REPLACE "\n" ";" TAGS_LIST ${TAGS_STRING})
+
+            # find the version tag
+            foreach(tag ${TAGS_LIST})
+                string(REGEX MATCH ${VERSION_REGEX} GIT_TAG_VERSION ${tag})
+                if(GIT_TAG_VERSION)
+                    break()
+                endif()
+            endforeach()
+        endif()
+
+        # sanitize the version tag to only be numbers delimited by "."
+        string(REGEX REPLACE "[^0-9.]" "" PROJECT_REVISION "${GIT_TAG_VERSION}")
+        # persist the version tag
+        set(PROJECT_REVISION "${PROJECT_REVISION}" CACHE INTERNAL "")
+    endfunction()
+
+    # get the latest tag
     execute_process(
         COMMAND ${GIT_EXECUTABLE} describe --tags --abbrev=0
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         OUTPUT_VARIABLE GIT_TAG_VERSION
         OUTPUT_STRIP_TRAILING_WHITESPACE
     )
+
+    if(NOT GIT_TAG_VERSION)
+        set(GIT_TAG_VERSION "0.0.1")
+    endif()
+
+    # find the appropriate version tag
+    findVersionTag(${GIT_TAG_VERSION})
 
     option(GIT_SUBMODULE "Check submodules during build" ON)
     if(GIT_SUBMODULE)
@@ -21,8 +73,6 @@ if(GIT_FOUND)
     endif()
 
 else()
-    set(GIT_TAG_VERSION "1.0.0")
+    # if git is not found, default to 0.0.1
+    set(PROJECT_REVISION "0.0.1")
 endif()
-
-# sanitize the version to prevent cmake errors
-string(REGEX REPLACE "[^0-9.]" "" SANITIZED_VERSION "${GIT_TAG_VERSION}")
